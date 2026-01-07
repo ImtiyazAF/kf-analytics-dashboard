e5996e4ca449017aa57afd572016aafd254ce66c
ImtiyazAF <5026231197@student.its.ac.id>
2026-01-07 09:27:00 +0700

    add folder

---
+++ sql/01_master_table_creation.sql
@@ -0,0 +1,18 @@
+CREATE TABLE kimia_farma.analytics(
+  transaction_id STRING,
+  date DATE,
+  branch_id INT64,
+  branch_name STRING,
+  kota STRING,
+  provinsi STRING,
+  rating_cabang FLOAT64,
+  customer_name STRING,
+  product_id STRING,
+  product_name STRING,
+  actual_price INT64,
+  discount_percentage FLOAT64,
+  persentase_gross_laba FLOAT64,
+  nett_sales FLOAT64,
+  nett_profit FLOAT64,
+  rating_transaksi FLOAT64
+)
